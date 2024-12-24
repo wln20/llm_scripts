56d416f0e5257085ea3a07ed5ba89945af4b455d
Luning Wang <wangluning2@gmail.com>
2024-12-24 11:17:38 -0500

    Create 4_trim_context_length.py

---
+++ langchain_examples/chatbot/4_trim_context_length.py
@@ -0,0 +1,84 @@
+"""
+Multi round dialogue with langgraph, support automatic memory and multiprocess conversation, and with customized prompt template and trimmed context length.
+Reference: https://python.langchain.com/docs/tutorials/chatbot/
+Dependencies:
+```
+pip install langchain-core, langgraph>0.2.27
+pip install -qU langchain-mistralai
+pip install transformers    # need tokenizer to trim the context
+```
+"""
+import os
+from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, trim_messages
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+# langgraph
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = # TODO
+os.environ["MISTRAL_API_KEY"] = # TODO
+
+
+model = ChatMistralAI(model="mistral-large-latest")
+
+# Define prompt template
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You talk like a pirate. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+# Define trim settings
+trimmer = trim_messages(
+    max_tokens=40,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human",
+)
+
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+
+
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    trimmed_messages = trimmer.invoke(state["messages"])    # trim the context
+    prompt = prompt_template.invoke({"messages": trimmed_messages})  # use prompt_template.invoke() to incorporate the template
+    response = model.invoke(prompt)
+    return {"messages": response}
+
+
+
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+# Use thread_id to identify which conversation is we on
+thread_id = "1"
+
+while True:
+    thread_id_new = input(f"Enter thread_id (Default to {thread_id}): ")
+    if thread_id_new and thread_id_new != thread_id:
+        thread_id = thread_id_new
+        print(f' ================= Change to thread {thread_id_new} ================= \n')
+
+    prompt = input("Human: ")
+    if prompt == 'q': 
+        break
+
+    config = {"configurable": {"thread_id": thread_id}}
+    input_messages = [HumanMessage(prompt)]
+    output = app.invoke({"messages": input_messages}, config)
+    print(f"AI({thread_id}):" + output["messages"][-1].content)  # output contains all messages in state
